68deabd880acd280e7169b9c029a0765d94bed6e
Yoona <zz@gmail.com>
2022-02-15 19:18:07 +0800

    1.0

---
+++ A1052/code.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+const int maxn=100010;
+
+struct node{
+    int addr;
+    int key;
+    int next;
+}link[maxn];
+
+ bool cmp(node a,node b){
+     return a.key<b.key;
+ }
+ int main(){
+     int n,head;
+     scanf("%d%d",&n,&head);
+     printf("%d ",n);
+     for(int i=0;i<n;i++){
+        scanf("%d %d %d",&link[i].addr,&link[i].key,&link[i].next);
+     }
+     sort(link,link+n,cmp);
+     printf("%d\n",link[0].addr);
+    for(int i=0;i<n;i++){
+        if(i!=n-1){
+            printf("%05d %d %05d\n",link[i].addr,link[i].key,link[i+1].addr);
+        }else{
+            printf("%05d %d %d\n",link[i].addr,link[i].key,-1);
+        }
+    }
+}
+ 
